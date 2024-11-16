05e62436bf2174afef917072b0e85ac00c8a7f0a
JeniferQ <144718154+JeniferQ@users.noreply.github.com>
2024-11-16 01:28:52 -0500

    Change page contents
    In this commit, both page contents were changed. Texts are no longer hidden and a banner container was added.

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
 <html lang="en">
-
+    
 <?php
 require_once('includes/connect.php');
 
 $query = 'SELECT employees.id AS employee, fname, lname, thumb FROM employees';
 
 $results = mysqli_query($connect, $query);
-print_r($results);
 ?>
 
 <head>
@@ -15,27 +13,44 @@ print_r($results);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="css/grid.css" rel="stylesheet" type="text/css">
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/main.css" rel="stylesheet" type="text/css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=DynaPuff:wght@400..700&display=swap" rel="stylesheet">
 
     <title>Simpsons Page</title>
 </head>
+
 <body>
     <main>
         <section class="full-width-grid-con banner">
-            <h1 class="hidden">Welcome to the Simpsons Employee Page</h1>
-            <img class="col-span-full" src="images/banner.jpg" alt="Simpsons Banner">
+            <div class="grid-con">
+                <img class="col-start-1 col-end-3 m-col-start-1 m-col-start-6" src="images/homer_face.jpg" alt="Simpsons image">
+                <h1 class="col-start-3 col-end-5 m-col-start-6 m-col-end-12 l-col-end-11">Welcome to the Simpsons Page</h1>
+            </div>
         </section>
 
         <section class="full-width-grid-con employee-section">
             <div class="grid-con">
-                <h2 class="col-span-full">Meet the Simpsons Employee Team</h2>
+                <h2 class="col-span-full">Learn who is behind each Simpson</h2>
 
+                <div class="col-span-full employee-cards">
                 <?php 
                     while($row = mysqli_fetch_array($results)) {
-                        echo '<div class="col-span-full employee-cards">
-                            <div class="card"><a href="details.php?id='.$row['employee'].'"><img src="images/'.$row['thumb'].'" alt="Employee image"></a><h3>'.$row['fname'].' '.$row['lname'].'</h3></div></div>';
+                    echo '<div class="card">
+                                <a href="details.php?id=';
+                                echo $row['employee'];
+                                echo '"><img src="images/';
+                                echo $row['thumb'];
+                                echo '" alt="Employee image"></a>
+                                <h3 class="box">';
+                                echo $row['fname'].' '.$row['lname'];
+                                echo '</h3>
+                            </div>';
                     }
                 ?>
+                </div>
+
             </div>
         </section>
     </main>
